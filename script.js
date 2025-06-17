2448d982398fbe9a91f1494eedb1870fa11f84b5
melaniec207 <carrillomelanie54@gmail.com>
2025-06-17 21:44:59 +0000

    selector red class

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ for(let i =0; i < items.length; i++)
     items[i].textContent = "Text change";
 }
 
+const header = document.querySelector("h1");
+console.log(header);
+const red = document.querySelectorAll(".redclass");
+console.log(red);
 /*"The Header"*/
 
 
