7ad25408f647aa06f238382132f411e79cedcf9f
melaniec207 <carrillomelanie54@gmail.com>
2025-06-17 21:13:17 +0000

    text change

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.getElementById("greeting").textContent = "Hi there!";
 document.getElementById("h1").textContent = "The Header";
+
+let items = document.getElementByIdClassname("redclass");
+for(let i =0; i < items.length; i++)
+{
+    items[i].textContent = "Text change";
+}
 /*"The Header"*/
 
 
