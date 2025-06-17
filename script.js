5d13505897f95dba3ff64015ce676c179bfa20f7
melaniec207 <carrillomelanie54@gmail.com>
2025-06-17 20:46:10 +0000

    fixed html and added to js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("greeting").textContent = "Hi there!";
+document.getElementById("h1").textContent = "The Header";
+/*"The Header"*/
+
+
+/*
+run this first: php -S localhost:3006 -t ./
+*/
