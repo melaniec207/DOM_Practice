a381c7aa452d517bceac73600d9f4979cff2872c
melaniec207 <carrillomelanie54@gmail.com>
2025-06-17 21:36:14 +0000

    fixed more bugs

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.getElementById("greeting").textContent = "Hi there!";
 document.getElementById("h1").textContent = "The Header";
 
-let items = document.getElementByIdClassname("redclass");
+let items = document.getElementsByClassName("redclass");
 for(let i =0; i < items.length; i++)
 {
     items[i].textContent = "Text change";
 }
+
 /*"The Header"*/
 
 
